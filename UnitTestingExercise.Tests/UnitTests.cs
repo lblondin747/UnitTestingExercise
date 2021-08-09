2dac58527f981b71bd775fc52b84f3fd74c8c13a
Luke Blondin <lblondin747@gmail.com>
2021-08-09 13:59:44 -0500

    initial ommit

--- UnitTestingExercise.Tests/UnitTests.cs
+++ UnitTestingExercise.Tests/UnitTests.cs
@@ -6,72 +6,94 @@ namespace UnitTestingExercise.Tests
     public class UnitTests
     {
         [Theory]
-        [InlineData()]
+        [InlineData(1, 2, 3, 6)]
+        [InlineData(2,2,2,6)]
+        [InlineData(12,15,13,40)]
+        [InlineData(10,5,2,17)]
         public void Add(int num1, int num2, int num3, int expected)
         {
+            var unitTest = new UnitTestMethods();
             //Arrange
 
+            var actual = unitTest.Add(num1, num2, num3);
             //Act
 
+            Assert.Equal(expected, actual);
             //Assert
 
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(20,10,10)]
+        [InlineData(53,20,33)]
+        [InlineData(19,17,2)]
         public void Subtract(int minuend, int subtrhend, int expected)
         {
+            var unitTest = new UnitTestMethods();
             //Arrange
 
+            var actual = unitTest.Subtract(minuend,subtrhend);
             //Act
 
+            Assert.Equal(expected, actual);
             //Assert
-
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(2,2,4)]
+        [InlineData(4,2,8)]
+        [InlineData(12,12,144)]
+        [InlineData(7,6,42)]
         public void Multiply(int num1, int num2, int expected)
         {
+            var unitTest = new UnitTestMethods();
             //Arrange
 
+            var actual = unitTest.Multiply(num1,num2);
             //Act
 
+            Assert.Equal(expected, actual);
             //Assert
 
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(32,16,2)]
+        [InlineData(12,3,4)]
+        [InlineData(66,3,22)]
         public void Divide(int num1, int num2, int expected)
         {
+            var unitTest = new UnitTestMethods();
             //Arrange
 
+            var actual = unitTest.Divide(num1, num2);
             //Act
 
+            Assert.Equal(expected, actual);
             //Assert
 
         }
 
         [Fact]
-        public void YourMethodName()
+        public void MyAge()
         {
-            //Arrange
+            var unitTest = new UnitTestMethods();
 
-            //Act
+            var actual = unitTest.myAge();
 
-            //Assert
+            Assert.Equal(19, actual);
+
 
         }
 
         [Fact]
-        public void YourMethodName2()
+        public void myBirthYear()
         {
-            //Arrange
+            var unitTest = new UnitTestMethods();
 
-            //Act
+            var actual = unitTest.myBirthYear();
 
-            //Assert
+            Assert.Equal(2002, actual);
         }
     }
 }
